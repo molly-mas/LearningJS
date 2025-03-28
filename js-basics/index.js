c61ebab8744e77ffa0b497d136acc5e189a98858
molly-mas <mollymasalskis@gmail.com>
2025-03-27 20:36:49 -0500

    Update index.js

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,2 +1,20 @@
 //Comment this is my first js code!
-console.log("hello world!");
+//console.log("hello world!");
+
+// you can use let or var to declare a variable but its recommended to use let
+let name = "MM";
+console.log(name);
+
+// nameing conventions: cant be reserved keyword (lowkey duh) 
+// names should be meaningful not x, y ect...
+// cant start with numbers in js, ex: 1name
+// cant contain spaces or hyphens (-) 
+// js uses cammelCase
+// variable names are case sensitive
+// you can techincally instanciate variables on the same line but dont for readability
+
+let firstName = "Molly"
+let lastName = "Masalskis"
+
+console.log(firstName);
+console.log(lastName);
